90d364e40648aaa193d2ef3663d4e09b7a095ec7
gopisankar6 <gopisankar6@gmail.com>
2025-07-23 17:49:41 +0300

    new

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import pyodbc
+
+app = Flask(__name__)
+
+# Azure SQL connection
+server = 'ynh-hmis-db.database.windows.net'
+database = 'ynh-hmis-db'
+username = 'dbadmin'
+password = 'yanbu@2025'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+def get_connection():
+    return pyodbc.connect(
+        f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    )
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ''
+    if request.method == 'POST':
+        try:
+            emp_id = int(request.form['id'])
+            emp_name = request.form['name']
+            emp_age = int(request.form['age'])
+
+            conn = get_connection()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO [ynh-prod-db] (id, name, age) VALUES (?, ?, ?)", 
+                           (emp_id, emp_name, emp_age))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            message = 'Employee data saved successfully.'
+        except Exception as e:
+            message = f'Error: {e}'
+    return render_template('form.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
